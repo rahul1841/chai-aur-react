f426e8b577f9cab4a7a5289235903bc6ab50352d
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-01-08 13:57:21 +0000

    done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./chai"
 
 function App() {
-
+const username = "chai aur code"
 
   return (
     <>
     <Chai/>
-    <h1>Chai aur react with vite |  RAHUL</h1>
+    <h1>Chai aur react with vite |  RAHUL {username}</h1>
     <p>code aur chai</p>
     </>
   )
